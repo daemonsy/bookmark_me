0afa2815496af4740b2179bf4bd8d285895f92be
Damon Aw <daemonsy@gmail.com>
2012-07-20 18:00:14 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarkme::Application.config.secret_token = '777fe67638dfbe60960824cdd716a94dccddf8997ec7f8b507516e4d2fe58d21f2a3fa4e72d0b8e075969e7590c50e814d16c230e776b9b0b7f2bb367d6759dd'
